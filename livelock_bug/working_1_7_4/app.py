eba3f1f7ccdd089c3e25baa433ecafea69192bd4
Sergej Herbert <herbert.sergej@gmail.com>
2019-10-19 11:33:47 +0200

    Add livelock bug example

---
+++ livelock_bug/working_1_7_4/app.py
@@ -0,0 +1,31 @@
+import faust
+
+
+class Greeting(faust.Record):
+    from_name: str
+    to_name: str
+
+
+app = faust.App('livelock-bug-working', broker='kafka://localhost')
+topic = app.topic('livelock_working', value_type=Greeting)
+app.conf.store = 'memory://'
+app.conf.web_port = 6099
+
+
+@app.agent(topic)
+async def hello(greetings):
+    async for greeting in greetings:
+        print(f'Hello from {greeting.from_name} to {greeting.to_name}')
+
+
+@app.timer(interval=1.0)
+async def example_sender(app):
+    await hello.send(
+        value=Greeting(from_name='Faust', to_name='you'),
+    )
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        sys.argv.extend(['worker', '-l', 'info'])
+    app.main()
